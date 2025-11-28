6f89a66f8fbc601884714d944f892cd6f3e378c6
jalloh19 <ikdiallotechie@gmail.com>
2025-11-28 13:13:20 +0800

    Flatten repository structure - remove nested alx_travel_app directory
    - Remove nested alx_travel_app/alx_travel_app/ structure
    - Rename Django project package to alx_travel_app_project/
    - All files now at repository root: manage.py, requirement.txt, listings/, etc.
    - Structure is now: alx_travel_app/manage.py (not alx_travel_app/alx_travel_app/manage.py)
    - Update manage.py, wsgi.py, asgi.py to reference alx_travel_app_project

--- alx_travel_app_project/settings.py
+++ alx_travel_app_project/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "alx_travel_app.urls"
+ROOT_URLCONF = "alx_travel_app_project.urls"
 
 TEMPLATES = [
     {
@@ -85,7 +85,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "alx_travel_app.wsgi.application"
+WSGI_APPLICATION = "alx_travel_app_project.wsgi.application"
 
 
 # Database
